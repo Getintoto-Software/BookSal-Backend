deb0b75f4f8c2998fa4a251fa353392be61275cd
Adamya Neupane <adamsneupane@gmail.com>
2024-12-11 12:44:50 +0545

    added user profile model fields

--- futsal/models.py
+++ futsal/models.py
@@ -34,6 +34,8 @@ class Futsal(models.Model):
     changing_room = models.BooleanField(default=False)
     restaurant = models.BooleanField(default=False)
     wifi = models.BooleanField(default=False)
+    # On false display indoor. On True display open ground
+    open_ground = models.BooleanField(default=False)
 
     futsal_description = models.TextField(default="")
 
@@ -48,18 +50,15 @@ class Futsal(models.Model):
 class Booking(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     futsal = models.ForeignKey(Futsal, on_delete=models.CASCADE)
-    booking_name = models.CharField(max_length=200)
-    phone_number = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     booking_date = models.DateField()
     booking_start_time = models.TimeField()
     booking_end_time = models.TimeField()
     # On false display pending. On True display confirmed
     booking_status = models.BooleanField(default=False)
-    # On false display indoor. On True display open ground
-    open_ground = models.BooleanField(default=False)
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.futsal.name + ' - ' + self.user.username + ' - ' + str(self.date)
+        return self.futsal.name + ' - ' + self.user.username + ' - ' + str(self.booking_date)
